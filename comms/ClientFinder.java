66a5fbffb0bdaf1eccfbde00e2ef76116a6f8b50
ThePersonOfStuff <maxf6.283@gmail.com>
2023-05-22 14:14:50 -0700

    asd

--- comms/ClientFinder.java
+++ comms/ClientFinder.java
@@ -31,7 +31,6 @@ public class ClientFinder implements Runnable {
     static {
         try {
             group = InetAddress.getByName("230.1.1.1");
-            networkInterface = NetworkInterface.getByName("eth2");
         } catch (IOException e) {
             e.printStackTrace();
         }
