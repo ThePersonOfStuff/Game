a9afcda9a8adfd1577d35e090e1b2009fda745ae
ThePersonOfStuff <maxf6.283@gmail.com>
2023-05-26 13:40:38 -0700

    .

--- comms/ClientFinder.java
+++ comms/ClientFinder.java
@@ -10,7 +10,6 @@ import java.net.Socket;
 import java.net.SocketTimeoutException;
 import java.net.UnknownHostException;
 import java.nio.charset.StandardCharsets;
-import java.util.Enumeration;
 
 import javax.swing.DefaultListModel;
 
